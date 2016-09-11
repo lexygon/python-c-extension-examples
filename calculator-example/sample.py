7f33bbe44c3ff6efca89691512e5d749474d0c93
lexygon <hi@buraktopal.xyz>
2016-09-12 01:30:24 +0300

    first example and init

---
+++ calculator-example/sample.py
@@ -0,0 +1,10 @@
+# importing the extension like a standart python module
+from calculator import *
+
+# calling functions from extension like a standart python function
+add = addition(3, 5)
+sub = substraction(10, 5)
+multi = multiplication(2, 4)
+div = division(20, 5)
+
+print(add, sub, multi, div)
---
+++ calculator-example/setup.py
@@ -0,0 +1,23 @@
+"""
+This file has been written with love by Burak Topal (lexygon) for giving a simple example for writing C extensions for Python 3.5.
+Feel free to cloning, sharing, editing and committing some new examples.
+I have tried to explain each part basicly as I can.
+For communicating with me:
+mail: hi@buraktopal.xyz
+github: github.com/lexygon
+"""
+
+from distutils.core import setup, Extension
+
+# defining calculator_module as an extension class instance.
+# 'calculator' is extension's name and sources is a file name list.
+calculator_module = Extension('calculator', sources=['calculator.c'])
+
+# calling setup function with theese parameters.
+setup(name='python_c_calculator_extension',
+      version='0.1',
+      description='An Example For Python C Extensions',
+      ext_modules=[calculator_module],
+      url='github.com/lexygon',
+      author='Burak Topal',
+      author_email='hi@buraktopal.xyz')
